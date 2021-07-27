fd656993c684f229071fc320dad15c36acaf8315
Pablo Justino <pabskne@gmail.com>
2021-07-27 17:36:52 -0300

    Login screen created

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,66 @@
+import createTheme from '@material-ui/core/styles/createTheme'
+import { ThemeProvider } from '@material-ui/core/styles'
 import React from 'react'
 import { AuthProvider } from './modules/useAuth'
+import { CssBaseline } from '@material-ui/core'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#890ACB',
+      main: '#6F09B2',
+      dark: '#3B0680',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#DD0BE3',
+      main: '#C00BE3',
+      dark: '#A20BE3',
+      contrastText: '#fff'
+    },
+    type: 'dark'
+  },
+  overrides: {
+    MuiOutlinedInput: {
+      input: {
+        '&:-webkit-autofill': {
+          color: '#fff !important',
+          appearance: 'none',
+          caretColor: '#6F09B2',
+          WebkitTextFillColor: '#fff !important',
+          backgroundColor: '#DD0BE3 !important',
+          WebkitBoxShadow: "0 0 0 1000px #424242 inset",
+        },
+      },
+    },
+    MuiFilledInput: {
+      input: {
+        '&:-webkit-autofill': {
+          color: '#fff !important',
+          appearance: 'none',
+          borderTopColor: '#535353',
+          borderBottomColor: '#535353',
+          borderLeftColor: '#535353',
+          borderRightColor: '#535353',
+          caretColor: '#6F09B2',
+          boxShadow: '0 0 0 1000px #535353 inset !important',
+          WebkitTextFillColor: '#fff !important',
+          backgroundColor: '#535353 !important',
+          WebkitBoxShadow: "0 0 0 1000px #535353 inset",
+        },
+      },
+    },
+  },
+})
 
 function App(): any {
   return (
-    <AuthProvider>
-      <>Bem vindo</>
-    </AuthProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AuthProvider>
+        <div>#TODO</div>
+      </AuthProvider>
+    </ThemeProvider>
   )
 }
 
--- front/src/modules/useAuth/Auth.tsx
+++ front/src/modules/useAuth/Auth.tsx
@@ -1,30 +1,55 @@
 import React from 'react'
 
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
-import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog'
-import ListItemText from '@material-ui/core/ListItemText'
-import ListItem from '@material-ui/core/ListItem'
-import List from '@material-ui/core/List'
-import Divider from '@material-ui/core/Divider'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import IconButton from '@material-ui/core/IconButton'
-import Typography from '@material-ui/core/Typography'
-import CloseIcon from '@material-ui/icons/Close'
-import Slide from '@material-ui/core/Slide'
+import TextField from '@material-ui/core/TextField'
 import { TransitionProps } from '@material-ui/core/transitions'
 
 import { AuthContext } from './authContext'
+import { Button, FormControl, Grid, Paper, Zoom } from '@material-ui/core'
+
+import Blob from './blob.svg'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    appBar: {
+    dialog: {
+      height: '100%',
+      width: '100%',
       position: 'relative'
     },
-    title: {
-      marginLeft: theme.spacing(2),
-      flex: 1
+    paper: {
+      width: '100%',
+      height: '100%',
+      padding: theme.spacing(2)
+    },
+    form: {
+      height: '100%',
+      width: '100%'
+    },
+    container: {
+      height: '100%'
+    },
+    item: {
+      textAlign: 'center'
+    },
+    input: {
+      width: '50%'
+    },
+    background: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      opacity: 0.5,
+      backgroundColor: '#000'
+    },
+    backgrondSvg: {
+      position: 'absolute',
+      top: 50,
+      left: 0,
+      width: '100%',
+      height: '100%'
     }
   })
 )
@@ -33,7 +58,7 @@ const Transition = React.forwardRef(function Transition(
   props: TransitionProps & { children?: React.ReactElement },
   ref: React.Ref<unknown>
 ) {
-  return <Slide direction="up" ref={ref} {...props} />
+  return <Zoom ref={ref} {...props} />
 })
 
 const _ = () => {
@@ -50,40 +75,97 @@ const _ = () => {
   return (
     <Dialog
       fullScreen
+      className={classes.dialog}
       open={authState.authOpen}
       onClose={close}
       TransitionComponent={Transition}
     >
-      <AppBar className={classes.appBar}>
-        <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            onClick={close}
-            aria-label="close"
+      <div className={classes.background}>
+        <img className={classes.backgrondSvg} src={Blob} alt="blob" />
+      </div>
+      <Paper className={classes.paper}>
+        <form
+          className={classes.form}
+          onSubmit={e => {
+            e.preventDefault()
+            close()
+          }}
+        >
+          <Grid
+            container
+            className={classes.container}
+            spacing={3}
+            style={{ height: '100%' }}
+            alignContent="center"
+            alignItems="center"
           >
-            <CloseIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Sound
-          </Typography>
-          <Button autoFocus color="inherit" onClick={close}>
-            save
-          </Button>
-        </Toolbar>
-      </AppBar>
-      <List>
-        <ListItem button>
-          <ListItemText primary="Phone ringtone" secondary="Titania" />
-        </ListItem>
-        <Divider />
-        <ListItem button>
-          <ListItemText
-            primary="Default notification ringtone"
-            secondary="Tethys"
-          />
-        </ListItem>
-      </List>
+            <Grid item xs={12} className={classes.item}>
+              <h2>Login</h2>
+            </Grid>
+            <Grid
+              container
+              item
+              xs={12}
+              justify="center"
+              className={classes.item}
+            >
+              <Grid item xs={12} md={6}>
+                <TextField
+                  required
+                  fullWidth
+                  autoFocus
+                  margin="normal"
+                  variant="filled"
+                  type="text"
+                  label="Nickname"
+                  id="nickname"
+                  name="nickname"
+                  autoComplete="nickname"
+                />
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              item
+              xs={12}
+              justify="center"
+              className={classes.item}
+            >
+              <Grid item xs={12} md={6}>
+                <TextField
+                  required
+                  fullWidth
+                  margin="normal"
+                  variant="filled"
+                  type="password"
+                  id="password"
+                  name="password"
+                  label="Password"
+                  autoComplete="password"
+                />
+              </Grid>
+            </Grid>
+            <Grid
+              container
+              item
+              xs={12}
+              justify="center"
+              className={classes.item}
+            >
+              <Grid item xs={12} md={6}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  fullWidth
+                >
+                  Login
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
     </Dialog>
   )
 }
